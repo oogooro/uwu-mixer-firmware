2e1df0ed15087a1d96a7173a4478172e01888342
oogooro <70372978+oogooro@users.noreply.github.com>
2025-02-24 20:09:03 +0100

    Dynamic analog channels

--- include/config.h
+++ include/config.h
@@ -4,3 +4,6 @@
 #define PROTOCOL_VERSION 1
 #define MAX_SENSOR_VALUE 1023
 #define FEATURES 0
+#define NUM_OF_CHANNELS 5
+
+const uint8_t channelPins[NUM_OF_CHANNELS] = {A4, A3, A2, A1, A0};
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 #include "config.h"
 
-bool sensorsEnabled[NUM_ANALOG_INPUTS] = {};
+bool sensorsEnabled[NUM_OF_CHANNELS] = {};
 uint8_t filterThreshold = 5;
+uint32_t poolingRate = 30; // in Hz
 
 void setup() {
-  for (int i = 0; i < NUM_ANALOG_INPUTS; i++) {
-    pinMode(A0 + i, INPUT);
+  for (uint8_t channelPin : channelPins) {
+    pinMode(channelPin, INPUT);
   }
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -15,21 +16,22 @@ void setup() {
   Serial.print(":");
   Serial.print(PROTOCOL_VERSION);
   Serial.print(":");
-  Serial.print(NUM_ANALOG_INPUTS);
+  Serial.print(NUM_OF_CHANNELS);
   Serial.print(":");
   Serial.print(FEATURES);
   Serial.write('\n');
 }
 
-uint16_t pots[NUM_ANALOG_INPUTS] = {};
-uint8_t potsMapped[NUM_ANALOG_INPUTS] = {};
+uint16_t pots[NUM_OF_CHANNELS] = {};
+uint8_t potsMapped[NUM_OF_CHANNELS] = {};
+bool reversePots = false;
 
 bool readPots() {
   bool potsChanged = false;
 
-  for (uint8_t i = 0; i < NUM_ANALOG_INPUTS; i++) {
+  for (uint8_t i = 0; i < NUM_OF_CHANNELS; i++) {
     if (!sensorsEnabled[i]) continue;
-    uint16_t sensor = analogRead(A0 + i);
+    uint16_t sensor = analogRead(channelPins[i]);
     if ((abs(pots[i] - sensor) >= filterThreshold) && (potsMapped[i] != map(sensor, 0, MAX_SENSOR_VALUE, 0, 100))) {
       pots[i] = sensor;
       potsMapped[i] = map(pots[i], 0, MAX_SENSOR_VALUE, 0, 100);
@@ -40,7 +42,18 @@ bool readPots() {
   return potsChanged;
 }
 
+void sendPots() {
+  Serial.write('=');
+  for (uint8_t i = 0; i < NUM_OF_CHANNELS; i++) {
+    if (!sensorsEnabled[i]) Serial.write('-');
+    else Serial.print(reversePots ? 100 - potsMapped[i] : potsMapped[i]);
+    if (i != NUM_OF_CHANNELS - 1) Serial.print(':');
+  }
+  Serial.write('\n');
+}
+
 bool programingMode = false;
+bool commandMode = false;
 
 void loop() {
   while (Serial.available()) {
@@ -49,25 +62,44 @@ void loop() {
     if (incoming == '#') {
       programingMode = !programingMode;
       digitalWrite(LED_BUILTIN, programingMode);
-      break;
+
+      if (!programingMode) {
+        readPots();
+        sendPots();
+      }
+      continue;
     }
     
     if (programingMode) {
-      uint8_t index = incoming - '0';
+      if (incoming == 'r') {
+        reversePots = !reversePots;
+      } else {
+        uint8_t index = incoming - '0';
+        if (index < NUM_OF_CHANNELS) sensorsEnabled[index] = !sensorsEnabled[index];
+      }
+    }
 
-      if (index < NUM_ANALOG_INPUTS - 1) sensorsEnabled[index] = !sensorsEnabled[index];
+    if (incoming == '$') {
+      commandMode = true;
+      continue;
+    }
+
+    if (commandMode) {
+      if (incoming == '\n') {
+        commandMode = false;
+        continue;
+      } else if (incoming == 'e') {
+        commandMode = false;
+        Serial.write('*');
+        for (uint8_t i = 0; i < NUM_OF_CHANNELS; i++) {
+          Serial.write(sensorsEnabled[i] ? 'e' : 'd');
+        }
+        Serial.write('\n');
+      }
     }
   }
   
-  if (!programingMode && readPots()) {
-    Serial.write('=');
-    for (uint8_t i = 0; i < NUM_ANALOG_INPUTS; i++) {
-      if (!sensorsEnabled[i]) Serial.write('-');
-      else Serial.print(potsMapped[i]);
-      if (i != NUM_ANALOG_INPUTS - 1) Serial.print(':');
-    }
-    Serial.write('\n');
-  }
+  if (!programingMode && readPots()) sendPots();
   
-  delay(1);
+  delay(1000 / poolingRate);
 }
